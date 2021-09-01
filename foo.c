d948315f5f0523b0001761822670127d88330588
Toshiya Saito <saitou@ialab.cs.tsukuba.ac.jp>
2021-09-01 23:00:59 +0900

    foo.cを削除

--- foo.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int foo() { printf("foo\n"); return 2; }
-int bar(int x, int y) { printf("%d\n", x + y); return x + y; }
-int goo(int x, int y, int z, int s, int t, int u) {
-	printf("%d\n", x + y + z + s + t + u);
-	return (x + y + z + s + t + u);
-}
